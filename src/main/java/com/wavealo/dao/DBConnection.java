62880de5b23d846ffbaa8c10574b520e231accdd
Luca <bottoneluca327@gmail.com>
2026-05-22 12:41:31 +0200

    feat(dao): add DBConnection utility class
    Entity class for the connection with the database.

---
+++ src/main/java/com/wavealo/dao/DBConnection.java
@@ -0,0 +1,15 @@
+package com.wavealo.dao;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+public class DBConnection {
+    public static Connection getConnection() throws Exception{
+        Context initContext = new InitialContext();
+        Context envContext = (Context) initContext.lookup("java:comp/env");
+        DataSource ds = (DataSource) envContext.lookup("jdbc/wavealo");
+        return ds.getConnection();
+    }
+}
