9582f5fefbaf6bc95e64b981cf331acb67d62f1a
Luca <bottoneluca327@gmail.com>
2026-05-26 16:39:03 +0200

    feat(model): add CartItem class

---
+++ src/main/java/com/wavealo/model/CartItem.java
@@ -0,0 +1,29 @@
+package com.wavealo.model;
+
+public class CartItem {
+    private Prodotto prodotto;
+    private int quantita;
+
+    public CartItem() {}
+
+    public CartItem(Prodotto prodotto, int quantita) {
+        this.prodotto = prodotto;
+        this.quantita = quantita;
+    }
+
+    public Prodotto getProdotto() {
+        return prodotto;
+    }
+
+    public void setProdotto(Prodotto prodotto) {
+        this.prodotto = prodotto;
+    }
+
+    public int getQuantita() {
+        return quantita;
+    }
+
+    public void setQuantita(int quantita) {
+        this.quantita = quantita;
+    }
+}
